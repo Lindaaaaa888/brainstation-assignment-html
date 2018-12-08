d13e11b522546dbddf33e9db1715fd2834bb3070
Linda <heyixuan64@gmail.com>
2018-12-07 22:44:33 -0500

    first comit

---
+++ assignment-2.js
@@ -0,0 +1,12 @@
+var info = ""
+for (var i = 1; i <=7; i++){
+	info += "#"
+	console.log(info);
+}
+function isEven(num) {
+	if (!(num % 2)) {
+		console.log("the number is even");
+	} else {
+	console.log("the number is odd");
+	}
+}
